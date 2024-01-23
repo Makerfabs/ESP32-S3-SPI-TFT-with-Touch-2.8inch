e76820ecb55584aaf08ff01737bc451fb78a1b00
一九五 <2259761058@qq.com>
2024-01-23 14:07:26 +0800

    Add lvgl demo

---
+++ example/LvglWidgets/LvglWidgets.ino
@@ -0,0 +1,167 @@
+/*******************************************************************************
+ * LVGL Widgets
+ * This is a widgets demo for LVGL - Light and Versatile Graphics Library
+ * import from: https://github.com/lvgl/lv_demos.git
+ *
+ * Dependent libraries:
+ * LVGL: https://github.com/lvgl/lvgl.git
+
+ * Touch libraries:
+ * FT6X36: https://github.com/strange-v/FT6X36.git
+ * GT911: https://github.com/TAMCTec/gt911-arduino.git
+ * XPT2046: https://github.com/PaulStoffregen/XPT2046_Touchscreen.git
+ *
+ * LVGL Configuration file:
+ * Copy your_arduino_path/libraries/lvgl/lv_conf_template.h
+ * to your_arduino_path/libraries/lv_conf.h
+ * Then find and set:
+ * #define LV_COLOR_DEPTH     16
+ * #define LV_TICK_CUSTOM     1
+ *
+ * For SPI display set color swap can be faster, parallel screen don't set!
+ * #define LV_COLOR_16_SWAP   1
+ *
+ * Optional: Show CPU usage and FPS count
+ * #define LV_USE_PERF_MONITOR 1
+ ******************************************************************************/
+#include "lv_demo_widgets.h"
+
+#include <Arduino_GFX_Library.h>
+#include <bb_captouch.h>
+
+#define TFT_BLK 48
+#define TFT_RES -1
+
+#define TFT_CS 15
+#define TFT_MOSI 13
+#define TFT_MISO 12
+#define TFT_SCLK 14
+#define TFT_DC 21
+
+#define TOUCH_INT 40
+#define TOUCH_SDA 39
+#define TOUCH_SCL 38
+#define TOUCH_RST 1
+
+#define SCREEN_W 320
+#define SCREEN_H 240
+
+Arduino_ESP32SPI *bus = new Arduino_ESP32SPI(TFT_DC, TFT_CS, TFT_SCLK, TFT_MOSI, TFT_MISO, HSPI, true); // Constructor
+Arduino_GFX *gfx = new Arduino_ST7789(bus, TFT_RES, 1 /* rotation */, true /* IPS */);
+BBCapTouch bbct;
+
+/* Change to your screen resolution */
+static uint32_t screenWidth;
+static uint32_t screenHeight;
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t *disp_draw_buf;
+static lv_disp_drv_t disp_drv;
+
+/* Display flushing */
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
+{
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+#if (LV_COLOR_16_SWAP != 0)
+  gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#else
+  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#endif
+
+  lv_disp_flush_ready(disp);
+}
+
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+{
+
+  uint16_t x = 0, y = 0;
+  if (get_touch(&x, &y))
+  {
+    data->state = LV_INDEV_STATE_PR;
+
+    /*Set the coordinates*/
+    data->point.x = x;
+    data->point.y = y;
+  }
+  else
+  {
+    data->state = LV_INDEV_STATE_REL;
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("LVGL Widgets Demo");
+
+  pinMode(TFT_BLK, OUTPUT);
+  digitalWrite(TFT_BLK, 1);
+
+  // Init touch device
+  bbct.init(TOUCH_SDA, TOUCH_SCL, TOUCH_RST, TOUCH_INT);
+
+  // Init Display
+  gfx->begin();
+
+  lv_init();
+
+  screenWidth = SCREEN_W;
+  screenHeight = SCREEN_H;
+#ifdef ESP32
+  disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * 10, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+#else
+  disp_draw_buf = (lv_color_t *)malloc(sizeof(lv_color_t) * screenWidth * 10);
+#endif
+  if (!disp_draw_buf)
+  {
+    Serial.println("LVGL disp_draw_buf allocate failed!");
+  }
+  else
+  {
+    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * 10);
+
+    /* Initialize the display */
+    lv_disp_drv_init(&disp_drv);
+    /* Change the following line to your display resolution */
+    disp_drv.hor_res = screenWidth;
+    disp_drv.ver_res = screenHeight;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+    lv_disp_drv_register(&disp_drv);
+
+    /* Initialize the (dummy) input device driver */
+    static lv_indev_drv_t indev_drv;
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = my_touchpad_read;
+    lv_indev_drv_register(&indev_drv);
+
+    lv_demo_widgets();
+
+    Serial.println("Setup done");
+  }
+}
+
+void loop()
+{
+  lv_timer_handler(); /* let the GUI do its work */
+  delay(5);
+}
+
+int get_touch(uint16_t *x, uint16_t *y)
+{
+  TOUCHINFO ti;
+  if (bbct.getSamples(&ti))
+  {
+    *x = map(ti.y[0], 0, 320, 0, 320);
+    *y = map(ti.x[0], 240, 0, 0, 240);
+
+    // *x = ti.x[0];
+    // *y = ti.y[0];
+
+    return 1;
+  }
+  else
+    return 0;
+}
